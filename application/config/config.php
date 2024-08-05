c80aac185457c371e3e6cd17b74112d41c7d85c8
Md Asif Raza <63058117+asifrtu@users.noreply.github.com>
2024-08-06 05:24:58 +0530

    Update config.php
    update the config file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://hrhelp24x7.com/hrportal';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
